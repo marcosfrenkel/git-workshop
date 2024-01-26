597810fbb3564718f5bf29b8dffb739094b89e05
marcosf2 <marcosf2@illinois.edu>
2024-01-26 10:04:41 -0600

    Added extra people

--- generate_folders.sh
+++ generate_folders.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the list of names
-names="Abdullah Ale Jasper Kaushik Michael Oliver Philip Sam Sonia Supriya Wolfgang extra_1 extra_2"
+names="Abdullah Ale Jasper Kaushik Michael Oliver Philip Sam Sonia Supriya Wolfgang extra_1 extra_2 Randy Aayam Wei-Hsiang"
 
 # Loop over the names and create a directory for each one
 for name in $names
