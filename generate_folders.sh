ea966c2616814beb21953344963a6d6754bd48c5
marcosf2 <marcosf2@illinois.edu>
2024-01-26 01:39:40 -0600

    changed increment for print

---
+++ generate_folders.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Define the list of names
+names="Abdullah Ale Jasper Kaushik Michael Oliver Philip Sam Sonia Supriya Wolfgang extra_1 extra_2"
+
+# Loop over the names and create a directory for each one
+for name in $names
+do
+    mkdir $name
+    echo "def add(a, b):
+    result = a + b + 1
+    print(result)" > $name/add_and_print.py
+done
